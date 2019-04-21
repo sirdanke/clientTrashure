05e25a84fa77a53eda951da1da927330147648bb
sirdanke <ahmad.syukron6991@gmail.com>
2019-04-21 15:17:55 +0700

    api

--- trashure/Actions/Api.js
+++ trashure/Actions/Api.js
@@ -1,38 +1,46 @@
 import axios from 'axios'
-let url = 'http://localhost:3000'
+let url = 'http://35.237.249.100'
 export function UserLogin(email, password) {
     return dispatch => {
-        dispatch({ type : 'IS_LOADING'})
-
-       return  axios
-            .get(`http://localhost:3000/user?email=${email}`)
-            .then(({data})=> {
-                dispatch({type : 'LOGIN_SUCCESS'})
+        dispatch({ type: 'IS_LOADING' })
+        return axios
+            .get(`${url}`)
+            .then(({ data }) => {
+                dispatch({ type: 'LOGIN_SUCCESS' })
             })
-            .catch(({response})=> {
+            .catch(({ response }) => {
                 console.log(response, "=====");
-                
+
                 // dispatch({type : 'LOGIN_FAILED', payload: response.data.message})
             })
     }
 }
 
 export function sendRawData(object) {
-    console.log(object.latitude,"===masuk");
-    
+    console.log(JSON.stringify(object.base64), "===masuk");
+
     return dispatch => {
-        axios
-            .post(`http://localhost:3000/`, {
-                path : object.base64,
-                location : object.latitude
-            })
-            .then(({data})=> {
-                console.log(data, "=====");
-                
-                dispatch({type : 'SUCCESS_SEND_DATA'})
-            })
-            .catch(({response})=> {
-                // dispatch({type})
-            })
+        try {
+            axios
+                .post(`${url}/`, { path: object.base64 },
+                    {
+                        headers: {
+                            Token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVjYmMwYzY3OTQ5ZmY4MGVkOWE3MWY4YSIsIm5hbWUiOiJBZGkgUHJhc2V0eWEgUHV0cmEiLCJlbWFpbCI6ImFkaXBAbWFpbC5jb20iLCJpYXQiOjE1NTU4Mjc4NDN9.a-DhYHcovPC44zmRtPh0K3nUK4nRVsM_yxj1uhWIlzY'
+                        }
+                    })
+                .then(({ data }) => {
+                    console.log(data, "==DATA===");
+
+                    dispatch({ type: 'SUCCESS_SEND_DATA' })
+                })
+                .catch(error => {
+                    // console.log(JSON.stringify(error.response), "===ERROR==");
+
+                    // dispatch({type})
+                })
+        } catch (error) {
+            console.log(error, "===ERROR CATCH===");
+
+        }
     }
 }
